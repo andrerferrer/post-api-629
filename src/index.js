056c2c925e4536678f3cac2015ce0791bb3b39cd
andre ferrer <andrerferrer@hotmail.com>
2021-05-26 06:03:58 -0300

    finish lovecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('hello from index.js')
+const searchAlgoliaPlaces = (event) => {
+  const url = "https://places-dsn.algolia.net/1/places/query";
+
+  // console.log(event);
+  // console.log(event.currentTarget);
+  // console.log(event.currentTarget.value);
+
+  const bodyData = JSON.stringify(
+    { query: event.currentTarget.value }
+  );
+
+  const options = {
+    method: "POST",
+    body: bodyData
+  };
+
+  console.log(options);
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.hits);
+      console.log(data.hits[0]);
+      console.log(data.hits[0].locale_names);
+      console.log(data.hits[0].locale_names.default);
+      console.log(data.hits[0].locale_names.default[0]);
+      const cityName = data.hits[0].locale_names.default[0];
+      const resultP = document.querySelector('#result');
+      resultP.innerText = cityName;
+    });
+};
+
+const input = document.querySelector("#search");
+input.addEventListener("keyup", searchAlgoliaPlaces);
